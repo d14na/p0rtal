1b17865dc83506c32f32f2115c60c5578944220a
D14na <support@d14na.org>
2018-09-30 11:21:55 -0400

    Begin staging for Clearnet.

--- js/p0rtal-home.js
+++ js/p0rtal-home.js
@@ -1,6 +1,14 @@
-/* Load ZeroEffect. */
-App.cmd('fileGet', ['components/zeroeffect.html'], (_body) => {
-    $("#zeroeffect").html(_body)
+/**
+ * JQuery says we are good-to-go.
+ * So let's GO!
+ */
+$(async function() {
+    /* Initialize body holder. */
+    let body = null
+
+    /* Load ZeroEffect. */
+    body = await App.cmd('fileGet', 'components/zeroeffect.html')
+    $('#zeroeffect').html(body)
 
     var rs3 = new Rickshaw.Graph({
         element: document.querySelector('#rs3'),
@@ -38,140 +46,131 @@ App.cmd('fileGet', ['components/zeroeffect.html'], (_body) => {
         })
         rs3.render()
     })
-})
 
+    // var newCust = [[0, 2], [1, 3], [2,6], [3, 5], [4, 7], [5, 8], [6, 10]];
+    // var retCust = [[0, 1], [1, 2], [2,5], [3, 3], [4, 5], [5, 6], [6,9]];
+    //
+    // var plot = $.plot($('#flotArea1'),[
+    //     {
+    //         data: newCust,
+    //         label: 'New Customer',
+    //         color: '#1B84E7'
+    //     },
+    //     {
+    //         data: retCust,
+    //         label: 'Returning Customer',
+    //         color: '#4E6577'
+    //     }
+    // ],
+    // {
+    //     series: {
+    //         lines: {
+    //             show: true,
+    //             lineWidth: 0,
+    //             fill: 0.8
+    //         },
+    //         shadowSize: 0
+    //     },
+    //     points: {
+    //         show: false,
+    //     },
+    //     legend: {
+    //         noColumns: 1,
+    //         position: 'nw'
+    //     },
+    //     grid: {
+    //         hoverable: true,
+    //         clickable: true,
+    //         borderColor: '#ddd',
+    //         borderWidth: 0,
+    //         labelMargin: 5,
+    //         backgroundColor: '#fff'
+    //     },
+    //     yaxis: {
+    //         min: 0,
+    //         max: 15,
+    //         color: '#eee',
+    //         font: {
+    //             size: 10,
+    //             color: '#999'
+    //         }
+    //     },
+    //     xaxis: {
+    //         color: '#eee',
+    //         font: {
+    //             size: 10,
+    //             color: '#999'
+    //         }
+    //     }
+    // })
 
-// var newCust = [[0, 2], [1, 3], [2,6], [3, 5], [4, 7], [5, 8], [6, 10]];
-// var retCust = [[0, 1], [1, 2], [2,5], [3, 3], [4, 5], [5, 6], [6,9]];
-//
-// var plot = $.plot($('#flotArea1'),[
-//     {
-//         data: newCust,
-//         label: 'New Customer',
-//         color: '#1B84E7'
-//     },
-//     {
-//         data: retCust,
-//         label: 'Returning Customer',
-//         color: '#4E6577'
-//     }
-// ],
-// {
-//     series: {
-//         lines: {
-//             show: true,
-//             lineWidth: 0,
-//             fill: 0.8
-//         },
-//         shadowSize: 0
-//     },
-//     points: {
-//         show: false,
-//     },
-//     legend: {
-//         noColumns: 1,
-//         position: 'nw'
-//     },
-//     grid: {
-//         hoverable: true,
-//         clickable: true,
-//         borderColor: '#ddd',
-//         borderWidth: 0,
-//         labelMargin: 5,
-//         backgroundColor: '#fff'
-//     },
-//     yaxis: {
-//         min: 0,
-//         max: 15,
-//         color: '#eee',
-//         font: {
-//             size: 10,
-//             color: '#999'
-//         }
-//     },
-//     xaxis: {
-//         color: '#eee',
-//         font: {
-//             size: 10,
-//             color: '#999'
-//         }
-//     }
-// })
-
-$.plot("#flotBar1", [{
-    data: [[0, 3], [2, 8], [4, 5], [6, 13],[8,5], [10,7],[12,4], [14,6]]
-}], {
-    series: {
-        bars: {
-            show: true,
-            lineWidth: 0,
-            fillColor: '#4E6577'
+    $.plot('#flotBar1', [{
+        data: [[0, 3], [2, 8], [4, 5], [6, 13],[8,5], [10,7],[12,4], [14,6]]
+    }], {
+        series: {
+            bars: {
+                show: true,
+                lineWidth: 0,
+                fillColor: '#4E6577'
+            }
+        },
+        grid: {
+            borderWidth: 1,
+            borderColor: '#D9D9D9'
+        },
+        yaxis: {
+            tickColor: '#d9d9d9',
+            font: {
+                color: '#666',
+                size: 10
+            }
+        },
+        xaxis: {
+            tickColor: '#d9d9d9',
+            font: {
+                color: '#666',
+                size: 10
+            }
         }
-    },
-    grid: {
-        borderWidth: 1,
-        borderColor: '#D9D9D9'
-    },
-    yaxis: {
-        tickColor: '#d9d9d9',
-        font: {
-            color: '#666',
-            size: 10
-        }
-    },
-    xaxis: {
-        tickColor: '#d9d9d9',
-        font: {
-            color: '#666',
-            size: 10
-        }
-    }
-})
+    })
 
-/* Load zerolit. */
-App.cmd('fileGet', ['components/zerolit.html'], (_body) => {
-    $("#zerolit").html(_body)
+    /* Load zerolit. */
+    body = await App.cmd('fileGet', 'components/zerolit.html')
+    $('#zerolit').html(body)
 
     // Donut chart
+    // NOTE requires zerolit.html
     $('.peity-donut').peity('donut')
-})
 
-/* Load zerolife. */
-App.cmd('fileGet', ['components/zerolife.html'], (_body) => {
-    $("#zerolife").html(_body)
-})
+    /* Load zerolife. */
+    body = await App.cmd('fileGet', 'components/zerolife.html')
+    $('#zerolife').html(body)
 
-/* Load zerotasks. */
-App.cmd('fileGet', ['components/zerotasks.html'], (_body) => {
-    $("#zerotasks").html(_body)
-})
+    /* Load zerotasks. */
+    body = await App.cmd('fileGet', 'components/zerotasks.html')
+    $('#zerotasks').html(body)
 
-/* Load recent-messages. */
-App.cmd('fileGet', ['components/recent-messages.html'], (_body) => {
-    $("#recent-messages").html(_body)
-})
+    /* Load recent-messages. */
+    body = await App.cmd('fileGet', 'components/recent-messages.html')
+    $('#recent-messages').html(body)
 
-/* Load new-topic. */
-App.cmd('fileGet', ['components/new-topic.html'], (_body) => {
-    $("#new-topic").html(_body)
-})
+    /* Load new-topic. */
+    body = await App.cmd('fileGet', 'components/new-topic.html')
+    $('#new-topic').html(body)
 
-/* Load carousel. */
-App.cmd('fileGet', ['components/carousel.html'], (_body) => {
-    $("#carousel").html(_body)
-})
+    /* Load carousel. */
+    body = await App.cmd('fileGet', 'components/carousel.html')
+    $('#carousel').html(body)
 
-/* Load trending-stories. */
-App.cmd('fileGet', ['components/trending-stories.html'], (_body) => {
-    $("#trending-stories").html(_body)
-})
+    /* Load trending-stories. */
+    body = await App.cmd('fileGet', 'components/trending-stories.html')
+    $('#trending-stories').html(body)
 
-/* Load sponsor. */
-App.cmd('fileGet', ['components/sponsor.html'], (_body) => {
-    $("#sponsor").html(_body)
-})
+    /* Load sponsor. */
+    body = await App.cmd('fileGet', 'components/sponsor.html')
+    $('#sponsor').html(body)
 
-/* Load feedback. */
-App.cmd('fileGet', ['components/feedback.html'], (_body) => {
-    $("#feedback").html(_body)
+    /* Load feedback. */
+    body = await App.cmd('fileGet', 'components/feedback.html')
+    $('#feedback').html(body)
 })
--- js/p0rtal.js
+++ js/p0rtal.js
@@ -1,289 +1,261 @@
-'use strict';
-
 /**
- * ZeroApp
+ * ZeroApp Class
  *
  * Framework for allowing simple app development.
  */
-const _initApp = (function () {
-    const _hasProp = {}.hasOwnProperty
-    const _extend = function (_child, _parent) {
-        for (let key in _parent) {
-            if (_hasProp.call(_parent, key)) _child[key] = _parent[key]
-        }
+class ZeroApp extends ZeroApi {
+    /* Constructor. */
+    constructor(_url) {
+        super(_url)
 
-        function _ctor() {
-            this.constructor = _child
-        }
-
-        _ctor.prototype = _parent.prototype;
-        _child.prototype = new _ctor()
-        _child.__super__ = _parent.prototype
-
-        return _child
+        this.saveData = this.saveData.bind(this)
+        this.selectUser = this.selectUser.bind(this)
     }
 
-    /* Initialize ZeroApp object (class). */
-    const ZeroApp = (function (superClass) {
-        _extend(ZeroApp, superClass)
+    /**
+     * Open WebSocket Connection
+     *
+     * When the Zeronet page is loaded a websocket is openend.
+     * This can be used to check if the user is logged in.
+     */
+    async onOpen(_event) {
+        /* Call super. */
+        super.onOpen()
 
-        /* Constructor. */
-        function ZeroApp() {
-            this.saveData = this.saveData.bind(this)
-            this.selectUser = this.selectUser.bind(this)
+        /* Request server info. */
+        const serverInfo = await this.cmd('serverInfo', {})
+        // console.info('Server Info', serverInfo)
 
-            return ZeroApp.__super__.constructor.apply(this, arguments)
+        /* Initialize client version. */
+        const version = serverInfo.version
+
+        /* Initialize client revision. */
+        const rev = serverInfo.rev
+
+        /* Initialize TOR status. */
+        const tor = {
+            enabled: serverInfo.tor_enabled,
+            status: serverInfo.tor_status
         }
 
-        /**
-         * Open WebSocket
-         *
-         * When the Zeronet page is loaded a websocket is openend.
-         * This can be used to check if the user is logged in.
-         */
-        ZeroApp.prototype.onOpenWebSocket = async function (_event) {
-            /* Initialize the mobile viewport. */
-            this.cmd('wrapperSetViewport', 'width=device-width, initial-scale=1.0')
+        /* Initialize user language. */
+        const i18n = serverInfo.language
 
-            /* Request server info. */
-            const serverInfo = await this.cmd('serverInfo', {})
-            // console.info('Server Info', serverInfo)
+        /* Initialize user platform. */
+        const platform = serverInfo.platform
 
-            /* Initialize client version. */
-            const version = serverInfo.version
+        /* Initialize client plugins. */
+        const plugins = serverInfo.plugins
 
-            /* Initialize client revision. */
-            const rev = serverInfo.rev
+        /* Request site info. */
+        const siteInfo = await this.cmd('siteInfo', {})
+        // console.info('Site Info', siteInfo)
 
-            /* Initialize TOR status. */
-            const tor = {
-                enabled: serverInfo.tor_enabled,
-                status: serverInfo.tor_status
-            }
+        /* Initialize # of peers. */
+        const numPeers = siteInfo.peers
 
-            /* Initialize user language. */
-            const i18n = serverInfo.language
+        /* Set # of peers to UI. */
+        $('.numPeers').html(numPeers)
 
-            /* Initialize user platform. */
-            const platform = serverInfo.platform
+        /* Initialize settings. */
+        const settings = siteInfo.settings
+        // console.info('Site Settings', settings)
 
-            /* Initialize client plugins. */
-            const plugins = serverInfo.plugins
-
-            /* Request site info. */
-            const siteInfo = await this.cmd('siteInfo', {})
-            // console.info('Site Info', siteInfo)
-
-            /* Initialize # of peers. */
-            const numPeers = siteInfo.peers
-
-            /* Set # of peers to UI. */
-            $('.numPeers').html(numPeers)
-
-            /* Initialize settings. */
-            const settings = siteInfo.settings
-            console.log('SETTINGS', settings)
-
-            if (!settings || settings.permissions.indexOf('ADMIN') === -1) {
-                App.cmd('wrapperNotification', ['error', 'Oops! You need to enable ADMIN to use this P0rtal..', 9000])
-            }
-        }
-
-        /**
-         * Route
-         *
-         * Messages received in real-time are managed here.
-         * (It can be used to login the user once he selects a certificate.)
-         */
-        ZeroApp.prototype.route = function (_cmd, _message) {
-            console.log('Received incoming routed message', _cmd, _message)
-
-            // if (_cmd === 'setSiteInfo') {
-            //     /* Set site info. */
-            //     this.siteInfo = _message.params
-            //
-            //     /* Verify user status. */
-            //     if (_message.params.cert_user_id) {
-            //         return this._log(`User is logged in as [ ${message.params.cert_user_id} ]`)
-            //     } else {
-            //         return this._log('User is NOT logged in.')
-            //     }
-            // }
-        }
-
-        /**
-         * Select User
-         *
-         * Displays the certificate selection dialog box to the user.
-         *
-         * Use it from your html files with an onclick attribute:
-         * <a href="#" onclick="return App.selectUser()">Login</a>
-         */
-        ZeroApp.prototype.selectUser = function () {
-            /* Initialize accepted domains. */
-            // NOTE ENS = Ethereum Name Service (https://ens.domains/)
-            const acceptedDomains = {
-                'accepted_domains': [
-                    'ethnick.bit', // .ETH on ENS ID Provider
-                    'kxoid.bit', // KxoNetwork ID Provider
-                    'xyzid.bit', // .XYZ on ENS ID Provider
-                    'zeroid.bit' // ZeroNet Core ID Provider
-                ]
-            }
-
-            /* Request certificate provider from user. */
-            App.cmd('certSelect', { acceptedDomains })
-
-            return false
-        }
-
-        /**
-         * Update User Quota
-         *
-         * Users have a quota or limit on the data they can post to each site.
-         * This method gets the max_size allowed in this site and the current
-         * user's data size.
-         */
-        ZeroApp.prototype.updateUserQuota = function () {
-            if (this.siteInfo.cert_user_id) {
-                /* Initialize path to the user's data file. */
-                const innerPath = 'data/users/' + this.siteInfo.auth_address + '/content.json'
-
-                return this.cmd('fileRules', innerPath, (_rules) => {
-                    this._log(`Current / max size [ ${_rules.current_size} / ${_rules.max_size} ]`)
+        /* Validate the ADMIN permission. */
+        if (!settings || settings.permissions.indexOf('ADMIN') === -1) {
+            /* Initialize request button. */
+            $('.btnAdminRequest').click(() => {
+                /* Request ADMIN permission. */
+                App.cmd('wrapperPermissionAdd', ['ADMIN'], () => {
+                    /* Reload the page (from cache). */
+                    window.location.reload(false)
                 })
-            }
-        }
+            })
 
-        /**
-         * Query Data
-         *
-         * Zeronet includes an SQLite database you can use to query your
-         * application data.
-         * NOTE This database is Read-Only.
-         */
-        ZeroApp.prototype.queryData = function () {
-            /*******************************************************************
-             Query ALL available user data from a ZeroApp.
-
-             query = ```
-                SELECT
-                    zeroapp.*, keyvalue.value AS cert_user_id
-                FROM
-                    zeroapp
-                LEFT JOIN
-                    json AS data_json USING (json_id)
-                LEFT JOIN
-                    json AS content_json ON (
-                        data_json.directory = content_json.directory AND
-                        content_json.file_name = 'content.json'
-                    )
-                LEFT JOIN
-                    keyvalue ON (
-                        keyvalue.key = 'cert_user_id' AND
-                        keyvalue.json_id = content_json.json_id
-                    )
-                ORDER BY date_added
-             ```
-            *******************************************************************/
-            const query = 'SELECT * FROM zeroapp'
-
-            return this.cmd('dbQuery', [query], (_data) => {
-                this._log(_data)
+            /* Show ADMIN permission modal. */
+            $('#modalAdminPermission').modal({
+                backdrop: 'static',
+                keyboard: false
             })
         }
+    }
 
-        /**
-         * Save Data
-         *
-         * JSON files are used to save or update the data in Zeronet.
-         */
-        ZeroApp.prototype.saveData = function () {
-            let innerPath
+    /**
+     * Event Message
+     *
+     * Messages received in real-time are managed here.
+     * (It can be used to login the user once he selects a certificate.)
+     */
+    onEvent(_cmd, _message) {
+        console.log('Received incoming routed message', _cmd, _message)
 
-            // NOTE The user needs to be logged in, in order to
-            //      post any data to the zite.
-            if (!App.siteInfo.cert_user_id) {
-                App.cmd('wrapperNotification', ['info', 'Please, select your account.'])
+        // if (_cmd === 'setSiteInfo') {
+        //     /* Set site info. */
+        //     this.siteInfo = _message.params
+        //
+        //     /* Verify user status. */
+        //     if (_message.params.cert_user_id) {
+        //         return this._log(`User is logged in as [ ${message.params.cert_user_id} ]`)
+        //     } else {
+        //         return this._log('User is NOT logged in.')
+        //     }
+        // }
+    }
 
-                return false
-            }
+    /**
+     * Select User
+     *
+     * Displays the certificate selection dialog box to the user.
+     *
+     * Use it from your html files with an onclick attribute:
+     * <a href="#" onclick="return App.selectUser()">Login</a>
+     */
+    selectUser() {
+        /* Initialize accepted domains. */
+        // NOTE ENS = Ethereum Name Service (https://ens.domains/)
+        const acceptedDomains = {
+            'accepted_domains': [
+                'ethnick.bit', // .ETH on ENS ID Provider
+                'kxoid.bit', // KxoNetwork ID Provider
+                'xyzid.bit', // .XYZ on ENS ID Provider
+                'zeroid.bit' // ZeroNet Core ID Provider
+            ]
+        }
 
+        /* Request certificate provider from user. */
+        App.cmd('certSelect', { acceptedDomains })
+
+        return false
+    }
+
+    /**
+     * Update User Quota
+     *
+     * Users have a quota or limit on the data they can post to each site.
+     * This method gets the max_size allowed in this site and the current
+     * user's data size.
+     */
+    updateUserQuota() {
+        if (this.siteInfo.cert_user_id) {
             /* Initialize path to the user's data file. */
-            innerPath = 'data/users/' + this.siteInfo.auth_address + '/data.json'
+            const innerPath = 'data/users/' + this.siteInfo.auth_address + '/content.json'
 
-            /* Load the current user's data file and push a new element. */
-            this.cmd('fileGet', {
-                'inner_path': innerPath,
-                'required': false
-            }, (_data) => {
-                let jsonRaw
-
-                if (_data) {
-                    // FIXME Add try-catch for parse
-                    _data = JSON.parse(_data)
-                } else {
-                    _data = {
-                        'zeroapp': []
-                    }
-                }
-
-                _data.zeroappp.push({
-                    'body': document.getElementById('message').value,
-                    'date_added': +(new Date)
-                })
-
-                jsonRaw = unescape(encodeURIComponent(JSON.stringify(data, void 0, '\t')))
-
-                /* Write file to disk. */
-                // NOTE All data posted by this user will be stored here.
-                //      Zeronet will automatically map it into the
-                //      SQLite database, so we can query it later.
-                return this.cmd('fileWrite', [_innerPath, btoa(jsonRaw)], (_res) => {
-                    /* Publish the file so other users can download it. */
-                    if (_res === 'ok') {
-                        return this.cmd('sitePublish', {
-                            'inner_path': _innerPath
-                        }, function (_innerRes) {
-                            return this._log('Data published successfully!')
-                        })
-                    } else {
-                        return _this.cmd('wrapperNotification', ['error', `Oops! There was an error writing the file [ ${_res}]`])
-                    }
-                })
+            return this.cmd('fileRules', innerPath, (_rules) => {
+                this._log(`Current / max size [ ${_rules.current_size} / ${_rules.max_size} ]`)
             })
+        }
+    }
+
+    /**
+     * Query Data
+     *
+     * Zeronet includes an SQLite database you can use to query your
+     * application data.
+     * NOTE This database is Read-Only.
+     */
+    queryData() {
+        /*******************************************************************
+         Query ALL available user data from a ZeroApp.
+
+         query = ```
+            SELECT
+                zeroapp.*, keyvalue.value AS cert_user_id
+            FROM
+                zeroapp
+            LEFT JOIN
+                json AS data_json USING (json_id)
+            LEFT JOIN
+                json AS content_json ON (
+                    data_json.directory = content_json.directory AND
+                    content_json.file_name = 'content.json'
+                )
+            LEFT JOIN
+                keyvalue ON (
+                    keyvalue.key = 'cert_user_id' AND
+                    keyvalue.json_id = content_json.json_id
+                )
+            ORDER BY date_added
+         ```
+        *******************************************************************/
+        const query = 'SELECT * FROM zeroapp'
+
+        return this.cmd('dbQuery', [query], (_data) => {
+            this._log(_data)
+        })
+    }
+
+    /**
+     * Save Data
+     *
+     * JSON files are used to save or update the data in Zeronet.
+     */
+    saveData() {
+        let innerPath
+
+        // NOTE The user needs to be logged in, in order to
+        //      post any data to the zite.
+        if (!App.siteInfo.cert_user_id) {
+            App.cmd('wrapperNotification', ['info', 'Please, select your account.'])
 
             return false
         }
 
-        /* Return the localized ZeroApp object for assignment. */
-        return ZeroApp
-    })(ZeroApi)
+        /* Initialize path to the user's data file. */
+        innerPath = 'data/users/' + this.siteInfo.auth_address + '/data.json'
 
-    /* Assign ZeroApp as a new "global" window variable. */
-    window.ZeroApp = ZeroApp
-}).call(this)
+        /* Load the current user's data file and push a new element. */
+        this.cmd('fileGet', {
+            'inner_path': innerPath,
+            'required': false
+        }, (_data) => {
+            let jsonRaw
+
+            if (_data) {
+                // FIXME Add try-catch for parse
+                _data = JSON.parse(_data)
+            } else {
+                _data = {
+                    'zeroapp': []
+                }
+            }
+
+            _data.zeroappp.push({
+                'body': document.getElementById('message').value,
+                'date_added': +(new Date)
+            })
+
+            jsonRaw = unescape(encodeURIComponent(JSON.stringify(data, void 0, '\t')))
+
+            /* Write file to disk. */
+            // NOTE All data posted by this user will be stored here.
+            //      Zeronet will automatically map it into the
+            //      SQLite database, so we can query it later.
+            return this.cmd('fileWrite', [_innerPath, btoa(jsonRaw)], (_res) => {
+                /* Publish the file so other users can download it. */
+                if (_res === 'ok') {
+                    return this.cmd('sitePublish', {
+                        'inner_path': _innerPath
+                    }, function (_innerRes) {
+                        return this._log('Data published successfully!')
+                    })
+                } else {
+                    return _this.cmd('wrapperNotification', ['error', `Oops! There was an error writing the file [ ${_res}]`])
+                }
+            })
+        })
+
+        return false
+    }
+}
 
 /* Initialize app. */
 const App = new ZeroApp()
-
-// app.cmd('wrapperNotification', ['info', 'Are you looking for this? Cause I found it!', 3000])
-// app.cmd('wrapperNotification', ['done', 'You\'re all set!', 6000])
-// app.cmd('wrapperNotification', ['error', 'Oops! Try again...', 9000])
-
-// app.cmd('wrapperPermissionAdd', ['Merger:ADMIN'])
-
-// app.cmd('wrapperPrompt', ['Enter your private key:', 'password'], (_input) => {
-//     app.cmd('wrapperNotification', ['done', `Nice! I see you entered [${_input}]`, 7000])
-// })
-
-// app.cmd('certSelect', { 'accepted_domains': ['ethnick.bit', 'kxoid.bit', 'xyzid.bit', 'zeroid.bit'] })
 
 /**
  * JQuery says we are good-to-go.
  * So let's GO!
  */
-$(function() {
+$(async function() {
     // showing 2nd level sub menu while hiding others
     $('.sidebar-nav-link').on('click', function(e) {
         var subMenu = $(this).next()
@@ -322,18 +294,33 @@ $(function() {
 
     $('[data-toggle="tooltip"]').tooltip({ trigger: 'hover' })
 
+    /* Initialize body holder. */
+    let body = null
+
     /* Load header. */
-    App.cmd('fileGet', ['components/header.html'], (_body) => {
-        $("#header").html(_body)
-    })
+    body = await App.cmd('fileGet', 'components/header.html')
+    $('#header').html(body)
 
     /* Load navbar. */
-    App.cmd('fileGet', ['components/navbar.html'], (_body) => {
-        $("#navbar").html(_body)
-    })
+    body = await App.cmd('fileGet', 'components/navbar.html')
+    body = $('#navbar').html(body)
 
     /* Load footer. */
-    App.cmd('fileGet', ['components/footer.html'], (_body) => {
-        $("#footer").html(_body)
-    })
+    body = await App.cmd('fileGet', 'components/footer.html')
+    $('#footer').html(body)
+
+    /* Verify NO parent window! */
+    // if (window.self === window.top) {
+    //     console.log('NOTIFICATION', Notification)
+    //     if (Notification) {
+    //         if (Notification.permission !== 'granted') {
+    //             Notification.requestPermission()
+    //         }
+    //     } else {
+    //         alert('Desktop notifications not available in your browser. Try Chromium.');
+    //     }
+    // } else {
+    //     console.log('P0rtal is contained within another window')
+    // }
+
 })
