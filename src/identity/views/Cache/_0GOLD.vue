4a3f41811c00ea42593cea4889c1e7df4ce7c640
D14na Support <support@d14na.org>
2019-02-18 09:45:28 -0500

    First ZeroCache transfer completed successfully.

--- src/identity/views/Cache/_0GOLD.vue
+++ src/identity/views/Cache/_0GOLD.vue
@@ -53,7 +53,7 @@ const components = {
 export default {
     components,
     data: () => ({
-        txAmount: 0,
+        txAmount: 10**8,
         balance: 0
     }),
     computed: mapGetters([
@@ -67,38 +67,65 @@ export default {
         gotoCache() {
             this.updateIdentityScreenId('cache')
         },
-        send() {
-            console.log(`Sending ${this.txAmount} ether`)
-        },
-        async buy() {
-            console.log('buy some crypto')
+        async send() {
+            const to = '0xb07d84F2c5D8bE1f4a440173BC536E0B2ee3b05E'
 
-            // console.log('', this.account.privateKey)
-            // console.log('this.address', this.address)
-
-            // this.getBalance()
-
-            // The Contract interface
-            let abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}]
-
-            console.log('ETHERS', ethers)
+            console.log(`Sending ${this.txAmount} ether to ${to}`)
 
             // Connect to the network
-            let provider = ethers.getDefaultProvider('ropsten')
+            const provider = new ethers.providers.JsonRpcProvider(
+                'https://ropsten.infura.io/v3/773850fb37e546dca04e04faf7ba2c58')
 
             // The address from the above deployment example
-            let contractAddress = "0x079F89645eD85b85a475BF2bdc82c82f327f2932"
+            let contractAddress = '0xfe50c8e86dd7f7f4fb3d9f634662e753445f8b4c'
+
+            const abi = require('./abi/zeroCache')
+
+            let privateKey = this.account.privateKey
+            let wallet = new ethers.Wallet(privateKey, provider)
+
+            let contract = new ethers.Contract(contractAddress, abi, wallet)
+
+            let contractWithSigner = contract.connect(wallet)
+
+            let tx = await contractWithSigner.transfer(
+                to,
+                '0x079F89645eD85b85a475BF2bdc82c82f327f2932', // ZeroCache
+                this.txAmount
+            )
+
+            console.log('TX', tx, moment().format('lll'))
+
+            // wait for mining
+            await tx.wait()
+
+            console.log('ALL done!', moment().format('lll'))
+        },
+        buy() {
+            console.log('buy some crypto')
+
+            this.getBalance2()
+        },
+        async getBalance2() {
+            // Connect to the network
+            const provider = new ethers.providers.JsonRpcProvider(
+                'https://ropsten.infura.io/v3/773850fb37e546dca04e04faf7ba2c58')
+
+            // The address from the above deployment example
+            let contractAddress = '0xfe50c8e86dd7f7f4fb3d9f634662e753445f8b4c'
+
+            const abi = require('./abi/zeroCache')
 
             // We connect to the Contract using a Provider, so we will only
             // have read-only access to the Contract
             let contract = new ethers.Contract(contractAddress, abi, provider)
 
-            let currentValue = await contract.balanceOf('0x399c0fA056E3cF7aeC4A9E0BDa47Ee014DE3a5F0')
+            let currentValue = await contract.balanceOf(
+                '0x079F89645eD85b85a475BF2bdc82c82f327f2932',
+                '0xe5Fe2e0Ec02bB85d0655CA6Cf4E23824fAD285DC'
+            )
 
-            console.log('CURRENT VALUE', currentValue)
-            console.log('CURRENT VALUE 2', currentValue.toString())
-            console.log('CURRENT VALUE 3', currentValue.div(100000000))
-            console.log('CURRENT VALUE 4', currentValue.div(100000000).toString())
+            console.log('CURRENT_VALUE', currentValue, currentValue.toString())
 
             let balance = currentValue.div(100000000).toString()
             this.balance = numeral(balance).format('0,0')
